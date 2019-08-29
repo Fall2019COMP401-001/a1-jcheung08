d3e38ef7c9f1f65c6a4a6acdbc59b7e1080b9210
josephcheung <josephcheung@152.23.132.103>
2019-08-29 19:01:36 -0400

    Java version commented out

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -14,7 +14,22 @@ public class A1Novice {
 		String lastName = "Brownstein";
 		double total = 9.05;
 		
-		System.out.println(firstName.charAt(0) + "." + lastName + ":" + total);
+		System.out.println(firstName.charAt(0) + "." + lastName + ": " + total);
+		
+		String firstNamee = "Corin";
+		String lastNamee = "Tucker";
+		double totall = 4.55;
+		
+		System.out.println(firstNamee.charAt(0) + "." + lastNamee + ": " + totall);
+		
+		String firstNameee = "Janet";
+		String lastNameee = "Weiss";
+		double totalll = 12.50;
+		
+		System.out.println(firstNameee.charAt(0) + "." + lastNameee + ": " + totalll);
+		
+		
+		
 		
 		
 		
