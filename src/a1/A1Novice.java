793ff3b96702aec1ed8a56f1e473146c45b7b39b
josephcheung <josephcheung@192.168.1.6>
2019-09-01 00:45:30 -0400

    fixed edge case of multiple customers

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,42 +9,43 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 		
 		int customers = scan.nextInt();
+		
+		for (int a=0; a<customers; a++) {
+			String[] fullName = new String[2];
+			
+			for (int i=0; i<fullName.length; i++) {
+				fullName[i] = scan.next();
+			}
+			
+			// number of items bought by customer
+			int uniqueItems = scan.nextInt();
+			
+			// Initialize
+			
+			double[] itemPrice = new double[uniqueItems];
+			
+			
+			for (int i=0; i<itemPrice.length; i++) {
+				int quantity = scan.nextInt();
+				String useless = scan.next();
+				double price = scan.nextDouble();
+				itemPrice[i] = quantity * price;
+			}
+			
+			
+			// Find total price
+			double totalSum = 0;
+			for (int i=0; i<itemPrice.length; i++) {
+				totalSum = totalSum + itemPrice[i];
+			}
+			
+			String conversion = String.format("%.2f", totalSum);
+			
+			
+			System.out.println(fullName[0].charAt(0) + ". " + fullName[1] + ": " + conversion);
+			
+		}
 	
-		String[] namesArray = new String[customers * 2];
-		
-		for (int i=0; i<namesArray.length; i++) {
-			namesArray[i] = scan.next();
-		}
-		
-		// number of items bought by customer
-		int uniqueItems = scan.nextInt();
-		
-		
-		
-		// Initialize
-		
-		double[] itemPrice = new double[uniqueItems];
-		
-		
-		for (int i=0; i<itemPrice.length; i++) {
-			int quantity = scan.nextInt();
-			String useless = scan.next();
-			double price = scan.nextDouble();
-			itemPrice[i] = quantity * price;
-		}
-		
-		
-		// Find total price
-		double totalSum = 0;
-		for (int i=0; i<itemPrice.length; i++) {
-			totalSum = totalSum + itemPrice[i];
-		}
-		
-		String conversion = String.format("%.2f", totalSum);
-		
-		scan.close();
-		System.out.println(namesArray[0].charAt(0) + ". " + namesArray[1] + ": " + conversion);
-		
 	}
 	
 }
