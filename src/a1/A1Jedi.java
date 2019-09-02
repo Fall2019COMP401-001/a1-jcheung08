f0ce972844a2fe5558b19f3803cea5237fabd58e
josephcheung <josephcheung@192.168.1.6>
2019-09-01 21:41:16 -0400

    initial testing of code

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -7,8 +8,55 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int items = scan.nextInt();
+		double[] price = new double[items];
+		String[] itemName = new String[items];
+		for (int i=0; i<items; i++) {
+			itemName[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		
+		
+		
+		int customers = scan.nextInt();
+		
+		String[] roster = new String[customers * 2];
+		
+		int[] numberOfCustomers = new int[itemName.length];
+		int[] quantityOfItems = new int[itemName.length];
+		
+		for (int c=0; c<customers; c++) {
+			
+			roster[c * 2] = scan.next();
+			roster[(c * 2) + 1] = scan.next();
+			
+			int boughtItems = scan.nextInt();
+			
+			
+			for (int z=0; z<boughtItems; z++) {
+				int quantity = scan.nextInt();
+				String food = scan.next();
+				for (int r=0; r<itemName.length; r++) {
+					if (food.equals(itemName[r])) {
+						numberOfCustomers[r] += 1;
+						quantityOfItems[r] += quantity;
+					}
+				}
+			}
+		}
+		
+		
+		
+		// printed output in for loop
+		
+		for (int p=0; p<itemName.length; p++) {
+			if (numberOfCustomers[p] == 0) {
+				System.out.println("No customers bought " + itemName[p]);
+			} else {
+				System.out.println(numberOfCustomers[p] + " customers bought " + 
+						quantityOfItems[p] + " " + itemName[p]);
+			}
+		}
 	}
 }
